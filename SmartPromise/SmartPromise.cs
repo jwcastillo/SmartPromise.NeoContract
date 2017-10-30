44f5f97c03ec6b6bf5d4547b17e240351ba49739
Sergey Ankarenko <AnkarenkoSergey@gmail.com>
2017-10-30 13:11:06 +0300

    init

---
+++ SmartPromise/SmartPromise.cs
@@ -0,0 +1,15 @@
+﻿using Neo.SmartContract.Framework;
+using Neo.SmartContract.Framework.Services.Neo;
+using System;
+using System.Numerics;
+
+namespace SmartPromise
+{
+    public class SmartPromise : SmartContract
+    {
+        public static void Main()
+        {
+            Storage.Put(Storage.CurrentContext, "Hello", "World");
+        }
+    }
+}
